8755638c840d4d78aaae5fbf25b71382df089d40
Preston Jones <preston.jones@gmx.de>
2024-07-20 02:22:00 +0200

    add contact-form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
+
 export class ContactComponent {
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  onSubmit() {
+    console.log(this.contactData);
+  }
 
 }
